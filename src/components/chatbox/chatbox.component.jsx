aa261dc3d2732dd6fe0ee7f2b1a466cca0677b8a
NamNS <apocalypsetank234@gmail.com>
2020-07-31 01:33:57 +0700

    NamNS: migrate chatbot part 1

--- src/components/chatbox/chatbox.component.jsx
+++ src/components/chatbox/chatbox.component.jsx
@@ -4,19 +4,80 @@ import FormInput from "../form-input/form-input.component";
 import Message from "../message/message.component";
 
 import "./chatbox.styles.scss";
+import Axios from "axios";
 
-const Chatbox = () => (
-    <div className="chatbox">
-        <div className="header">
-            <h3>Chatbox đây nè</h3>
-        </div>
-        <div className="message-box">
-            <FormInput type="text" placeholder="Message..." />
-            <span>
-                <i className="fa fa-paper-plane" aria-hidden="true"></i>
-            </span>
-        </div>
-        <div className="messages">
+
+// const Chatbox = () => (
+function Chatbox() {
+    const [msgs, setMsgs] = React.useState([]);
+    const [input, setInput] = React.useState("");
+    const [sessionId, setSessionId] = React.useState("123455678");
+
+    const msgClassify = ({ type, message, userName, customData }) => {
+        // console.log(msgs);
+        if (type === "receiver") {
+            return <Message
+                messages={message}
+                isMine
+            />;
+        } else if (type === "sender") {
+            return <Message messages={message} />;
+        }
+        // else if (type === "cardSender") {
+        //     return <SenderCardDiv message={message} username={userName} customData={customData} onSelectElement={handleElementChosen}/>;
+        // }
+    };
+
+    const sendMsg = (event) => {
+        if (event.key === "Enter") {
+            // connect.invoke("SendMessage", userName, input);
+            const inputObj = { type: "receiver", message: input, userName: "userName" };
+            setMsgs((msgs) => [...msgs, inputObj]);
+            const requestMsg = {
+                chatContent: [input],
+                sessionId: sessionId,
+                userName: "userName",
+            };
+            callAPI(requestMsg);
+            setInput("");
+        }
+    };
+
+    const callAPI = (message) => {
+
+        // Axios.post(`${ChatEndpoint}`, message)
+        Axios.post('https://bespoke-scent-8991.herokuapp.com/chat/dialogflow', message)
+            .then(response => {
+                console.log(response.data.response);
+                setSessionId(response.data.sessionId);
+
+                let res = {
+                    type: "sender",
+                    message: response.data.response,
+                    customData: []
+                };
+
+                setMsgs((msg) => [...msg, res]);
+            });
+
+    }
+
+    return (
+        <div className="chatbox">
+            <div className="header">
+                <h3>Chatbox đây nè</h3>
+            </div>
+            <div className="message-box">
+                {/* <FormInput type="text" placeholder="Message..."/> */}
+                <input type="text"
+                    onKeyPress={sendMsg}
+                    onChange={(event) => setInput(event.target.value)}
+                    value={input}></input>
+                <span>
+                    <i className="fa fa-paper-plane" aria-hidden="true"></i>
+                </span>
+            </div>
+            {/* <div className="messages">
             <Message
                 messages={[
                     "I want to book an perfume!",
@@ -56,8 +117,20 @@ const Chatbox = () => (
             />
             <Message messages={["Vơn!", "Cảm ơn bot!"]} isMine />
             <Message messages={["No problem!"]} />
+        </div> */}
+
+
+            <div className="messages">
+                {msgs.length > 0 ? (
+                    msgs.map((msg, index) => (
+                        <div key={index}>{msgClassify(msg)}</div>
+                    ))
+                ) : (
+                        <div></div>
+                    )}
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default Chatbox;
